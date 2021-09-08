c42a09934186afcfa2409f953c84272374d9cfa1
alonsandak <35176257+alonsandak@users.noreply.github.com>
2021-09-08 13:28:34 +0300

    refactore code

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-    Future<List<Post>> futurePost;
+  Future<List<Post>> futurePost;
 
   @override
   void initState() {
@@ -36,57 +36,126 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+
+
     return MaterialApp(
-      title: 'Search Profile Data ',
-      theme: ThemeData(
+        title: 'Search Profile Data ',
+        theme: ThemeData(
         primaryColor: Colors.lightBlueAccent,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Search Profile Analyzie '),
-        ),
-        body:
-        Container(
-          child: FutureBuilder<List<Post>>(
-            future: futurePost,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (_, index) => Container(
-                    child: Container(
-                      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                      padding: EdgeInsets.all(20.0),
-                      decoration: BoxDecoration(
-                        color: Color(0xff97FFFF),
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "${snapshot.data[index].title}",
-                            style: TextStyle(
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          Text("${snapshot.data[index].body}"),
-                        ],
-                      ),
-                    ),
-                  ),
-                );
-              } else {
-                return Center(child: CircularProgressIndicator());
-              }
-            },
+    ),
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+    appBar: AppBar(
+    title: Text('Search Profile Analyzie '),
+    ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(28.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // Top Widgets
+              Container(
+                width: double.infinity,
+                // color: Colors.green,
+                //height: _size.height * 0.25, // Take 25% width of the screen height
+                height  :    90.00 ,
+                child:
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      Expanded(
+                        child: TextFormField(
+                          obscureText: false,
+                          decoration: InputDecoration(hintText: 'Profile we Search '),
+                          onTap: (){print('API CALL HERE');},
+                        ),
+
+                      )
+                    ],
+                  )
+
+              ),
+
+              Expanded(
+                child:   Container(
+                  child: bulidlist(futurePost: futurePost),
+                ),
+              ),
+
+              // Bottom Widgets
+              Container(
+                width: double.infinity,
+                // color: Colors.blue,
+                height: 50.00, // Take 25% width of the screen height
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Profile sentiment is : ', style: TextStyle(fontSize: 23.0),),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
+    ),
+    );
+  }
+}
+
+class bulidlist extends StatelessWidget {
+  const bulidlist({
+    Key key,
+    @required this.futurePost,
+  }) : super(key: key);
+
+  final Future<List<Post>> futurePost;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Post>>(
+      future: futurePost,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return
+            Scrollbar(
+              child:
+              ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (_, index) => Container(
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  padding: EdgeInsets.all(20.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xff97FFFF),
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "${snapshot.data[index].title}",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text("${snapshot.data[index].body}"),
+                    ],
+                  ),
+                ),
+              ),
+          ),
+            );
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
